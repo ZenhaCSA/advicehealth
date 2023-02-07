5d1d399f555d4df7ca19d11a6b673b3e6ccc29a9
ZenhaCSA <118000009+ZenhaCSA@users.noreply.github.com>
2023-02-07 03:31:46 -0300

    navbar
    comecando a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+// import logo from '../src/a.png';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <img src='https://advicehealth.com.br/wp-content/uploads/2021/07/logo_advice_150.png' className="App-logo" alt="logo" /> */}
+        <img
+          decoding="async"
+          src="https://advicehealth.com.br/wp-content/uploads/2021/07/logo_advice_150.png"
+          alt="logo"
+          className="logoAH"
+        />
+        <div className="linksHome">
+          <a className="linksAba" href="#">Home</a>
+          <a className="linksAba" href="#">Agendamentos</a>
+          <a className="linksAba" href="#">Consultas</a>
+          {/* <i class="icon-search"></i> */}
+        </div>
       </header>
+      <body></body>
     </div>
   );
 }
