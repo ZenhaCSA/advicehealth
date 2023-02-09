4118d142e2283eb4e5cee395e3b9c7d5063f0494
ZenhaCSA <118000009+ZenhaCSA@users.noreply.github.com>
2023-02-09 20:45:41 -0300

    componentizacao de cards
    ajustes no componente cardAgendamento.
    Criacao de componente CardMedicos com dados de medicos.
    Criacao de objetos para consulta ( medicos e horarios).
    Ajustes de layout

--- src/Pages/Agendamentos/index.tsx
+++ src/Pages/Agendamentos/index.tsx
@@ -2,8 +2,11 @@ import Div from "../../Components/Styled/Containers/Div";
 import Grid from "../../Components/Styled/Containers/Grid";
 import CardMedicos from "../../Components/CardMedicos";
 import CardAgendamentos from "../../Components/CardAgendamentos";
+import horarios from "../../Components/Objects/horarios.json";
+import medicos from "../../Components/Objects/medicos.json";
 
 const Agendamentos = () => {
+
   return (
     <>
       <Div
@@ -18,45 +21,53 @@ const Agendamentos = () => {
             backgroundColor="rgb(221, 219, 219)"
             borderRadius="10px"
             p={{ lg: "10px" }}
-            maxHeight={{ lg: "10rem" }}
+            height={{ lg: "12.4%" }}
           >
             <Div
               mt={{ lg: "2px" }}
               overflow="scroll"
               overflowX="hidden"
-              maxHeight={{ lg: "10rem" }}
+              height={{ lg: "100%" }}
+              backgroundColor="rgb(221, 219, 219)"
             >
-              <CardMedicos />
+              {medicos.map((i) => (
+                <CardMedicos medico={i} />
+              ))}
             </Div>
             <Div
               backgroundColor="rgb(221, 219, 219)"
               borderRadius="10px"
               p={{ lg: "10px" }}
-              height={{ lg: "19.1rem" }}
+              height={{ lg: "100%" }}
               mt="30px"
             >
               <Div
                 mt={{ lg: "2px" }}
                 overflow="scroll"
                 overflowX="hidden"
-                maxHeight={{ lg: "10rem" }}
-              ></Div>
+                height={{ lg: "100%" }}
+              >
+                <Div>Visao</Div>
+              </Div>
             </Div>
           </Div>
           <Div
             backgroundColor="rgb(221, 219, 219)"
             borderRadius="10px"
-            p={{ lg: "10px" }}
-            height={{ lg: "32rem" }}
+            p={{ lg: "1rem" }}
+            height={{ lg: "25%" }}
             ml="30px"
           >
             <Div
               mt={{ lg: "2px" }}
+              pl={{ lg: "2.5%" }}
               overflow="scroll"
               overflowX="hidden"
-              maxHeight={{ lg: "10rem" }}
+              height={{ lg: "100%" }}
             >
-              <CardAgendamentos />
+              {horarios.map((i) => (
+                <CardAgendamentos horario={i} />
+              ))}
             </Div>
           </Div>
         </Grid>
