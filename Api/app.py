aefeab26733183e92f45580af5bd3fc10a466b1e
ZenhaCSA <118000009+ZenhaCSA@users.noreply.github.com>
2023-02-13 01:13:43 -0300

    api local
    Ajustes de pastas com separacao de objetos da aplicacao.
    Instalacao de axios para chamadas APIREST.
    Instalacao de flask cors para solucaop de erro de cors.
    ajustes parta chamadas diretas a api nova.

--- Api/app.py
+++ Api/app.py
@@ -1,45 +1,40 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
+
+with open("./Objects/medicos.json", encoding='utf-8') as listamedicos:
+    medicos = json.load(listamedicos)
+
+with open("./Objects/horarios.json", encoding='utf-8') as listahorarios:
+    horarios = json.load(listahorarios)
+
+with open("./Objects/agendamentoFeitos.json", encoding='utf-8') as listaagendamentos:
+    agendamentos = json.load(listaagendamentos)
 
 app = Flask(__name__)
+CORS(app)
 
-medicos = [
-    { 
-      "id": "1",
-      "nome": "Jose Geraldo",
-      "area": "Dentista" 
-    },
-    { 
-      "id": "2",
-      "nome": "Henrique Lacerda",
-      "area": "Neurologista" 
-    },
-    { 
-      "id": "3",
-      "nome": "Carlos Cardoso",
-      "area": "Podologo" 
-    },
-    { 
-      "id": "4",
-      "nome": "Laura Dayana",
-      "area": "Agronoma" 
-    },
-    { 
-      "id": "5",
-      "nome": "Ana Clara dos Santos",
-      "area": "Pediatra" 
-    }
-]
+# consultar medicos
 
-#consultar medicos
 
 @app.route('/medicos')
 def obter_medicos():
     return jsonify(medicos)
 
+
 @app.route('/medicos/<int:id>', methods=['GET'])
 def obter_medico(id):
-    for medico in medicos: 
+    for medico in medicos:
         if medicos.get('id') == id:
             return jsonify(medico)
+
+@app.route('/horarios', methods=['GET'])
+def obter_horarios():
+    return jsonify(horarios)
+
+@app.route('/agendamentos', methods=['GET'])
+def obter_agendamentos():
+    return jsonify(agendamentos)
+
 
 app.run(port=5000, host='localhost', debug=True)
